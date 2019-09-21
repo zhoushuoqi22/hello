0bc7fec5460c8d0cfffd6ed6ef91142c51fed42a
zhoushuoqi <zhoushuoqi@mininglamp.com>
2019-09-22 00:18:43 +0800

    hello

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -3,7 +3,7 @@ package hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello idea");
-        System.out.println("hello git");
+        System.out.println("hello");
 
     }
 
