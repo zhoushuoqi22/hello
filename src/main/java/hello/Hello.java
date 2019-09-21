910413c5a83f6a0f15c23bd1c072a7ba2e0659d5
zhoushuoqi22 <552320070@qq.com>
2019-09-22 00:18:21 +0800

    Update Hello.java

--- src/main/java/hello/Hello.java
+++ src/main/java/hello/Hello.java
@@ -2,9 +2,7 @@ package hello;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello idea");
-        System.out.println("hello git");
-
+        System.out.println("hello");
     }
 
     private void sayHi() {
